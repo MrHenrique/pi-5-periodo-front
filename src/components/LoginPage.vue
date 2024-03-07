147ea0858cab14ab08765a49c68a6d4d82babd92
MrHenrique <h_om_@hotmail.com>
2024-03-07 19:58:07 -0300

    Update LoginPage.vue

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -13,7 +13,7 @@ export default {
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img
         class="mx-auto h-10 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        src="https://tailwindui.com/img/logos/mark.svg?color=teal&shade=900"
         alt="Your Company"
       />
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
