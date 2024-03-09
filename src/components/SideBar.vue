ce2266659de65326440964bac664cc6cb3f91152
MrHenrique <h_om_@hotmail.com>
2024-03-08 21:41:45 -0300

    Update SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -23,15 +23,15 @@ function toggleMenu() {
           <font-awesome-icon
             v-if="!menuOpen"
             class="rounded-t-2xl bg-gray-950 p-2"
-            icon="fa-solid fa-arrow-up"
-          />
-          <font-awesome-icon
-            v-if="menuOpen"
-            class="rounded-t-2xl bg-gray-950 p-2"
-            icon="fa-solid fa-arrow-down"
+            icon="fa-solid fa-arrow-right"
           />
         </div>
         <div v-if="menuOpen" class="flex flex-col items-center">
+          <font-awesome-icon
+            v-if="menuOpen"
+            class="rounded-t-2xl bg-gray-950 p-2"
+            icon="fa-solid fa-arrow-left"
+          />
           <div class="flex border-gray-900">
             <img
               alt=""
@@ -40,19 +40,27 @@ function toggleMenu() {
             />
             <div class="pl-2">
               <p class="text-sm">
-                <strong class="block font-medium">Éder Santana</strong>
-                <span> Fazenda Unipam </span>
+                <strong class="block font-medium">Éder</strong>
               </p>
             </div>
           </div>
+<hr/>
           <div class="py-2"><span>Inicio</span></div>
           <div class="py-2"><span>Áreas</span></div>
           <div class="py-2"><span>Dispositivos</span></div>
           <div class="py-2"><span>Teste2</span></div>
-          <div class="py-2"><DarkMode /></div>
-          <div class="py-2"><font-awesome-icon icon="fa-solid fa-gears" /></div>
-          <div class="py-2"><font-awesome-icon icon="fa-solid fa-door-closed" /></div>
+<hr/>
+          <div class="py-2>
+          <div><DarkMode /></div>
+          <div><font-awesome-icon icon="fa-solid fa-gears" /></div>
+          <div><font-awesome-icon icon="fa-solid fa-door-closed" /></div></div>
         </div>
+      <div @click="toggleMenu">
+          <font-awesome-icon
+            v-else
+            class="rounded-t-2xl bg-gray-950 p-2"
+            icon="fa-solid fa-arrow-right"
+          />
       </div>
     </div>
   </div>
