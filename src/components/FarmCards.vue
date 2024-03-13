53986fe90053d97d7f1b26d06ce56ea801193767
Henrique <henrique.melo@crazytechlabs.com.br>
2024-03-13 12:28:06 -0300

    Resize as width

--- src/components/FarmCards.vue
+++ src/components/FarmCards.vue
@@ -1,27 +1,26 @@
 <template>
-  <div class="flex flex-wrap justify-start p-2 sm:p-12">
+  <div class="flex flex-wrap justify-center p-2 sm:p-12">
     <SearchBar class="w-full m-2" />
     <li
       v-for="(item, index) in items"
       :key="index"
-      class="li-farms bg-light text-dark dark:bg-dark-alt dark:text-light shadow-md overflow-hidden m-5 w-48"
+      class="li-farms bg-light text-dark dark:bg-dark-alt dark:text-light shadow-md overflow-hidden m-3 sm:m-6 w-72"
     >
-      <div class="p-4 flex flex-col items-center justify-between">
-        <div class="px-4 max-w-48">
-          <p class="text-4xl font-bold text-nowrap text-ellipsis ... overflow-hidden">
+      <div class="flex flex-col">
+        <div class="px-1 sm:px-4 pt-4 pb-2 max-w-72 items-start justify-between">
+          <p class="text-xl font-bold text-nowrap text-ellipsis ... overflow-hidden">
             {{ item.name }}
           </p>
         </div>
-        <div class="flex items-center m-1"></div>
-        <div class="flex items-center m-1">
-          <span class="material-icons">sensors</span><span class="ml-2">{{ item.devices }}</span>
-          <span class="material-icons"> place </span><span class="ml-2">{{ item.talhoes }}</span>
-        </div>
-        <div class="flex items-center m-1">
+        <div class="flex items-center justify-center px-1 sm:px-4 pt-2 pb-4 max-w-72">
+          <span v-if="!isMobile" class="material-icons">sensors</span>
+          <span v-if="!isMobile" class="ml-2">{{ item.devices }}</span>
+          <span v-if="!isMobile" class="material-icons"> place </span>
+          <span v-if="!isMobile" class="ml-2">{{ item.talhoes }}</span>
           <span class="material-icons"> thermostat </span>
-          <div class="uppercase tracking-wide text-sm font-semibold">{{ item.temperature }}°C</div>
+          <div class="uppercase">{{ item.temperature }}°C</div>
           <span class="material-icons"> water_drop </span>
-          <span class="tracking-wide text-sm font-semibold">{{ item.rain }} mm</span>
+          <span class="text-nowrap text-ellipsis ... overflow-hidden">{{ item.rain }} mm</span>
         </div>
       </div>
     </li>
@@ -30,6 +29,24 @@
 
 <script setup>
 import SearchBar from './SearchBar.vue'
+
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+
+const screenWidth = ref(window.innerWidth)
+const isMobile = ref(screenWidth.value < 402 ? true : false)
+
+const updateScreenWidth = () => {
+  screenWidth.value = window.innerWidth
+  isMobile.value = screenWidth.value < 402 ? true : false
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateScreenWidth)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateScreenWidth)
+})
 
 const items = [
   { name: 'Farm 1', temperature: 25, rain: 5, devices: 3, talhoes: 2 },
