67c973aa6d935ac134a0d9c68b134191c56a941d
Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
2024-03-17 16:33:35 -0300

    router and basic config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
+import FarmPage from '@/views/FarmPage.vue'
 
-const routes = [{ path: '/', component: HomePage }, { path: '/login' }]
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/login' },
+  { path: '/farm/:id?', component: FarmPage }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
---
+++ src/views/FarmPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container flex flex-col ">
+        <span>TESTE</span>
+        <span>TESTE</span>
+        <span>TESTE</span>
+        <span>TESTE</span>
+        <span>TESTE</span>
+        <span>TESTE</span>
+        <span>TESTE</span>
+        <span>TESTE</span>
+    </div>
+</template>
+
+<style scoped></style>
