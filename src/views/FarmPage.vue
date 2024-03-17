8aa6b873e033c6356576ecc99634a75b79129273
Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
2024-03-17 17:43:45 -0300

    basic map
    + array of markers (sensors that might be implemented on next updates)
    + farmpage added a communication with the component

--- src/views/FarmPage.vue
+++ src/views/FarmPage.vue
@@ -1,8 +1,6 @@
-<script setup>
-</script>
-
 <template>
     <div class="container flex flex-col ">
+        <FarmMap :markers="markers" />
         <span>TESTE</span>
         <span>TESTE</span>
         <span>TESTE</span>
@@ -13,5 +11,25 @@
         <span>TESTE</span>
     </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+import FarmMap from '@/components/FarmMap.vue';
+
+export default defineComponent({
+    name: 'FarmPage',
+    components: {
+        FarmMap
+    },
+
+    data() {
+        return {
+            markers: [
+                { lat: -18.599053568654103, long: -46.55767668414138, desc: "Sensor 1", type: "Umidade" },
+            ]
+        };
+    }
+});
+</script>
 
 <style scoped></style>
