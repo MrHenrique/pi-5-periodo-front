b4218c548fbed16dd323d5340086f07e540950b8
Henrique <henrique.melo@crazytechlabs.com.br>
2024-03-18 14:03:09 -0300

    New route + carrousel + new button

---
+++ src/components/HomePageCarousel.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="controls-carousel" class="relative" data-carousel="static">
+    <div class="h-56 overflow-hidden md:h-96">
+      <div
+        v-for="(item, index) in carouselItems"
+        :key="index"
+        :class="{
+          hidden: selectedIndex !== index,
+          'duration-700': true,
+          'ease-in-out': true
+        }"
+        data-carousel-item="active"
+        class="flex items-center justify-center"
+      >
+        <div class="text-center bg-light dark:bg-dark-alt m-6 w-[90%] h-80 rounded-3xl">
+          <p class="text-ellipsis overflow-hidden ...">{{ item.title }}</p>
+          <p class="text-ellipsis overflow-hidden ...">{{ item.content }}</p>
+        </div>
+      </div>
+    </div>
+    <button
+      type="button"
+      class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+      @click="prevSlide"
+    >
+      <span
+        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-light dark:bg-darker-grey group-hover:bg-light-alt dark:group-hover:bg-dark-alt group-focus:ring-2 group-focus:ring-primary group-focus:outline-none group-focus:text-primary"
+      >
+        <span class="material-icons hover:text-primary"> chevron_left </span>
+        <span class="sr-only">Voltar</span>
+      </span>
+    </button>
+    <button
+      type="button"
+      class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+      @click="nextSlide"
+    >
+      <span
+        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-light dark:bg-darker-grey group-hover:bg-light-alt dark:group-hover:bg-dark-alt group-focus:ring-2 group-focus:ring-primary group-focus:outline-none group-focus:text-primary"
+      >
+        <span class="material-icons hover:text-primary"> chevron_right </span>
+        <span class="sr-only">Próximo</span>
+      </span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedIndex: 0,
+      carouselItems: [
+        { title: 'Teste', content: 'TESTE' },
+        { title: 'teste2', content: 'TESTE2' }
+      ]
+    }
+  },
+  methods: {
+    prevSlide() {
+      this.selectedIndex =
+        (this.selectedIndex - 1 + this.carouselItems.length) % this.carouselItems.length
+    },
+    nextSlide() {
+      this.selectedIndex = (this.selectedIndex + 1) % this.carouselItems.length
+    }
+  }
+}
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,27 @@
 <script setup>
 import FarmCards from '@/components/FarmCards.vue'
+import HomePageCarousel from '@/components/HomePageCarousel.vue'
 </script>
 
 <template>
   <div class="container mx-auto">
-    <FarmCards />
+    <div class="mx-8 sm:mx-20 mt-4">
+      <HomePageCarousel />
+    </div>
+    <div class="mx-8 sm:mx-20 pt-8">
+      <div class="mx-8 flex flex-row justify-between items-center">
+        <span class="text-4xl">Fazendas</span>
+        <router-link to="/novaFazenda" class="flex items-center cursor-pointer group">
+          <span
+            class="material-icons hover:text-primary bg-light dark:bg-dark-alt group-hover:bg-light-alt dark:group-hover:bg-darker-grey p-1 text-lg rounded-lg"
+          >
+            add
+          </span></router-link
+        >
+      </div>
+
+      <FarmCards />
+    </div>
   </div>
 </template>
 
