04faeb70ddd64082c29b6eb8fb67fe2573194f53
Gustavo Silvério Rosa <105025089+GustavoSRos4@users.noreply.github.com>
2024-03-22 22:07:57 -0300

    +change leaflet to vueleaflet

--- src/components/FarmMap.vue
+++ src/components/FarmMap.vue
@@ -1,12 +1,23 @@
 <template>
-    <div id="mapContainer">
+    <div style="height:600px; width:1200px">
+
+        <l-map :useGlobalLeaflet="false" :zoom="15" :center="[-18.599053568654103, -46.55767668414138]">
+            <l-tile-layer url="https://tile.openstreetmap.de/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'></l-tile-layer>
+
+            <l-tile-layer url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png" layer-type="base" name="Topografia"
+                atribuition='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'></l-tile-layer>
+            <l-control-layers />
+           
+        </l-map>
     </div>
 </template>
 
 <script>
-import { defineComponent, onMounted, onBeforeMount } from 'vue';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import { defineComponent } from 'vue';
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LControlLayers, LMarker } from "@vue-leaflet/vue-leaflet";
+
 
 export default defineComponent({
     name: 'FarmMap',
@@ -17,42 +28,18 @@ export default defineComponent({
             default: () => []
         }
     },
-    setup(props) {
-        let map = null;
-
-        onMounted(() => {
-            createMapLayer();
-
-            if (props.markers.length) {
-                setMarkers()
-            }
-        });
-
-        onBeforeMount(() => {
-            if (map) {
-                map.remove();
-            }
-        })
-
-
-        const createMapLayer = () => {
-            map = L.map('mapContainer').setView([-18.599866771140512, -46.55585309779889], 15)
-            L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map)
-        }
-
-        const setMarkers = () => {
-            props.markers.map((markers) => {
-                return L.marker([markers.lat, markers.long], { title: 'Sensor', riseOnHover: true }).addTo(map)
-                    .bindPopup(markers.desc)
-            })
-        }
-    }
+        components: {
+        LMap,
+        LTileLayer,
+        LControlLayers,
+        LMarker,
+    },
+    data() {
+        return {
+            zoom: 2,
+        };
+    },
 })
 </script>
 
-<style scoped>
-#mapContainer {
-    width: 40rem;
-    height: 40rem;
-}
-</style>
+<style></style>
